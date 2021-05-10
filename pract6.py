f10a8c4fd378f5de6ec94e7ea34a13834b6e7d8e
Dasha <78652241+dasha-27@users.noreply.github.com>
2021-05-10 22:20:12 +0300

    Добавление pract6.py

--- pract6.py
+++ pract6.py
@@ -38,37 +38,16 @@ blue = 2
 pix_new = [[0]*3 for o in range(im.height)]
 
 for w in range(im.height):
-        y_red = data[w, :, red]
+    for e in range(3):
+        y = data[w, :, e]
         lm = linear_model.LinearRegression()
-        lm.fit(X, y_red)
-        predicted_red = lm.predict(X)
-        diff_red = y_red - predicted_red
-        diff_red = np.clip(diff_red, -threshold, threshold)
-        y_red = predicted_red + diff_red
-        y_red = np.clip(y_red, 0, 255)
-        pix_new[w][red] = y_red.astype(int)
-
-for r in range(im.height):
-        y_green = data[r, :, green]
-        lm = linear_model.LinearRegression()
-        lm.fit(X, y_green)
-        predicted_green = lm.predict(X)
-        diff_green = y_green - predicted_green
-        diff_green = np.clip(diff_green, -threshold, threshold)
-        y_green = predicted_green + diff_green
-        y_green = np.clip(y_green, 0, 255)
-        pix_new[r][green] = y_green.astype(int)
-
-for t in range(im.height):
-        y_blue = data[t, :, blue]
-        lm = linear_model.LinearRegression()
-        lm.fit(X, y_blue)
-        predicted_blue = lm.predict(X)
-        diff_blue = y_blue - predicted_blue
-        diff_blue = np.clip(diff_blue, -threshold, threshold)
-        y_blue = predicted_blue + diff_blue
-        y_blue = np.clip(y_blue, 0, 255)
-        pix_new[t][blue] = y_blue.astype(int)
+        lm.fit(X, y)
+        predicted = lm.predict(X)
+        diff = y - predicted
+        diff = np.clip(diff, -threshold, threshold)
+        y = predicted + diff
+        y = np.clip(y, 0, 255)
+        pix_new[w][e] = y.astype(int)
 
 pix = im.load()
 i = 0
